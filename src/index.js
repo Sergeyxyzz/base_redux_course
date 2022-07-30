a913f21c82dd4f2af63e80c6c3d87176efe6ef61
Sergey <sergeyxyzz@yandex.ru>
2022-07-30 15:01:10 +0300

    добавили actionCreator

--- src/index.js
+++ src/index.js
@@ -15,22 +15,27 @@ const reducer = (state = 0, action) => {
   }
 }
 const update = () => {
-  document.getElementById('counter').textContent = store.getState()
+  document.getElementById('counter').textContent = store.getState() // отрисовываем стейт в UI
 }
-const store = createStore(reducer) // createStore создает стор моего приложения, аргументом идет редьюсер
+
+const store = createStore(reducer) // createStore создает стор моего приложения, аргументом как манипулятор идет редьюсер
 store.subscribe(update) // подписались на отслеживание каждого действия в сторе, выводим подписку аргументом, в нашем случае апдейтом
 
+const inc = () => ({type: 'INC'}) // actioncreator
+const dec = () => ({type: 'DEC'})
+const rnd = (value) => ({type: 'RND', payload: value}) // payload нужен чтобы не фаршмачить в стэйте напрямую
+
 document.getElementById('inc').addEventListener('click', () => {
-  store.dispatch({type: 'INC'})
+  store.dispatch(inc())
 })
 
 document.getElementById('dec').addEventListener('click', () => {
-  store.dispatch({type: 'DEC'})
+  store.dispatch(dec())
 })
 
 document.getElementById('rnd').addEventListener('click', () => {
   const value = Math.floor(Math.random() * 10)
-  store.dispatch({type: 'RND', payload: value})
+  store.dispatch(rnd(value)) // выводим в UI значение value
 })
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
