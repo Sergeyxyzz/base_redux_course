57062fb9bbe0c3f9b7a549971640dad426ac368c
Sergey <sergeyxyzz@yandex.ru>
2022-07-30 15:11:42 +0300

    примитив стэйта заменили на объект

--- src/index.js
+++ src/index.js
@@ -2,20 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { legacy_createStore as createStore } from 'redux';
 
-const reducer = (state = 0, action) => {
+const initialState = {value: 0}
+
+const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'INC':
-      return state + 1;
+      return {
+        ...state, 
+        value: state.value + 1
+      };
     case 'DEC':
-      return state - 1;
+      return {
+        ...state, 
+        value: state.value - 1
+      };
     case 'RND':
-      return state * action.payload
+      return {
+        ...state, 
+        value: state.value * action.payload
+      };
     default:
       return state
   }
 }
 const update = () => {
-  document.getElementById('counter').textContent = store.getState() // отрисовываем стейт в UI
+  document.getElementById('counter').textContent = store.getState().value // отрисовываем стейт в UI
 }
 
 const store = createStore(reducer) // createStore создает стор моего приложения, аргументом как манипулятор идет редьюсер
