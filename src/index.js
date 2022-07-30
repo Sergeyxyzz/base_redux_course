b76e2f525f99a6f1099bbde65991d722fa063a87
Sergey <sergeyxyzz@yandex.ru>
2022-07-30 14:42:37 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { legacy_createStore as createStore } from 'redux';
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INC':
+      return state + 1;
+    case 'DEC':
+      return state - 1;
+    case 'RND':
+      return state * action.payload
+    default:
+      return state
+  }
+}
+const update = () => {
+  document.getElementById('counter').textContent = store.getState()
+}
+const store = createStore(reducer) // createStore создает стор моего приложения, аргументом идет редьюсер
+store.subscribe(update) // подписались на отслеживание каждого действия в сторе, выводим подписку аргументом, в нашем случае апдейтом
+
+document.getElementById('inc').addEventListener('click', () => {
+  store.dispatch({type: 'INC'})
+})
+
+document.getElementById('dec').addEventListener('click', () => {
+  store.dispatch({type: 'DEC'})
+})
+
+document.getElementById('rnd').addEventListener('click', () => {
+  const value = Math.floor(Math.random() * 10)
+  store.dispatch({type: 'RND', payload: value})
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <>
+    
+    </>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
